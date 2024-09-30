f91146a220ced49854eec70d688d77ffddce3597
venuvardhankanteti123 <119773983+venuvardhankanteti123@users.noreply.github.com>
2024-09-30 09:37:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,27 +20,46 @@ function loadCategory(categoryUrl, htmlUrl) {
               <div class="card h-100" id="card-${index}">
                 <img src="${item.image}" class="card-img-top" alt="${item.title}" onerror="this.style.display='none'">
                 <div class="card-body">
-                  <p class="card-title">${item.title.slice(0, 20)}</p>
-                  <h5 class="card-text">Price: $.${item.price}</h5>
-                  <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">More Info</a>
+                  <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-index="${index}">More Info</button>
                 </div>
               </div>
             `;
             div.appendChild(card);
           });
 
-         
-          const moreInfoButtons = document.querySelectorAll('.btn-primary');
-          moreInfoButtons.forEach((button, index) => {
-            button.addEventListener('click', (event) => {
-                        const cardId = event.target.closest('.card').id;
+          const modalHtml = `
+            <
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Product Details</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                    
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Add to cart</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          `;
 
-    
+          document.body.insertAdjacentHTML('beforeend', modalHtml);
+
+          const buttons = document.querySelectorAll('.btn-primary');
+          buttons.forEach((button) => {
+            button.addEventListener('click', (event) => {
+              const index = event.target.getAttribute('data-index');
               const modalBody = document.querySelector('.modal-body');
               modalBody.innerHTML = `
+                <img src="${data[index].image}" class="card-img-top" alt="${data[index].title}" onerror="this.style.display='none'">
                 <h5>Product Title: ${data[index].title}</h5>
                 <p>Product Description: ${data[index].description}</p>
-                <p>Product Price: Rs.${data[index].price}</p>
+                <p>Product Price: $.${data[index].price}</p>
               `;
             });
           });
@@ -48,47 +67,23 @@ function loadCategory(categoryUrl, htmlUrl) {
     });
 }
 
-
-const modalHtml = `
-  <!-- Modal -->
-  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Product Details</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <!-- modal content will be displayed here -->
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        </div>
-      </div>
-    </div>
-  </div>
-`;
-
-
-document.body.insertAdjacentHTML('beforeend', modalHtml);
-
 document.getElementById('jew').addEventListener('click', function() {
-  loadCategory('https:ucts/category/jewelery', 'jewel.html');
+  loadCategory('https://fakestoreapi.com/products/category/jewelery', 'jewel.html');
 });
 
 document.getElementById('men').addEventListener('click', function() {
-  loadCategory("https:ucts/category/men's%20clothing", 'men.html');
+  loadCategory("https://fakestoreapi.com/products/category/men's%20clothing", 'men.html');
 });
 
 document.getElementById('women').addEventListener('click', function() {
-  loadCategory("https:ucts/category/women's%20clothing", 'women.html');
+  loadCategory("https://fakestoreapi.com/products/category/women's%20clothing", 'women.html');
 });
 
 document.getElementById('ele').addEventListener('click', function() {
-  loadCategory('https:ucts/category/electronics', 'ele.html');
+  loadCategory('https://fakestoreapi.com/products/category/electronics', 'ele.html');
 });
 document.getElementById('cart').addEventListener('click', function() {
-  loadCategory('https:s', 'cart.html');
+  loadCategory('https://fakestoreapi.com/carts', 'cart.html');
 });
 
 document.getElementById('home').addEventListener('click', function() {
